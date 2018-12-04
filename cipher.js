21fb5b90a22182d295d038972b32c71a868f144a
ana galvez <a.galvezsalas@gmail.com>
2018-12-04 10:24:17 -0300

    codigo cipher funcionando

--- cipher.js
+++ cipher.js
@@ -1,8 +1,43 @@
 window.cipher = {
-  encode: () => {
+
+
+  encode: (mensaje_cifrar, offset) => {
     /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+
+    let cifrado = "";
+
+  for (let i = 0; i < mensaje_cifrar.length; i++) {
+
+      let ascii= mensaje_cifrar[i].charCodeAt();
+      if (ascii>=65 && ascii <=90) {                    //&& operador logico cuando ambos valores son verdaderos
+      let letracifrada = (ascii-65+offset)%26+65;    //formula de michelle
+      cifrado += String.fromCharCode(letracifrada);  //mediante este metodo obtengo lo cifrado en texto
+      }else {
+      cifrado += mensaje_cifrar[i];
   }
-};
+  }  
+    
+  return cifrado;
+
+     },
+
+
+  decode: (mensaje_cifrar, offset) => {
+    /* Acá va tu código */
+
+    let descifrado = "";
+
+    for (let i = 0; i < mensaje_cifrar.length; i++) {
+  
+        let ascii= mensaje_cifrar[i].charCodeAt();
+        if (ascii>=65 && ascii <=90) {                    
+        let letradescifrada = (ascii-65-offset)%26+65;    
+        descifrado += String.fromCharCode(letradescifrada);  
+        }else {
+          descifrado += mensaje_cifrar[i];
+    }
+    }  
+      
+    return descifrado;
+  
+       },}
