86ae665f069a826d1c0a00ffc601d4a97db7109f
ana galvez <a.galvezsalas@gmail.com>
2019-05-31 03:20:16 -0400

    subiendo cambios

--- src/cipher.js
+++ src/cipher.js
@@ -2,14 +2,14 @@ window.cipher = {
 
 
   encode: (mensaje_cifrar, offset) => {
-    /* Acá va tu código */
+
 
     let cifrado = "";
 
   for (let i = 0; i < mensaje_cifrar.length; i++) {
 
       let ascii= mensaje_cifrar[i].charCodeAt();
-      if (ascii>=65 && ascii <=90) {                    //&& operador logico cuando ambos valores son verdaderos
+      if (ascii>=65 && ascii <=90) {                    
       let letracifrada = (ascii-65+offset)%26+65;    //formula de michelle
       cifrado += String.fromCharCode(letracifrada);  //mediante este metodo obtengo lo cifrado en texto
       }else {
@@ -23,7 +23,7 @@ window.cipher = {
 
 
   decode: (mensaje_cifrar, offset) => {
-    /* Acá va tu código */
+  
 
     let descifrado = "";
 
